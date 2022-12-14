fc1293132ef7903f54be7fea14c9ba8a8355846b
hunghv <hunghv@vpbanks.com.vn>
2022-12-14 17:24:21 +0700

    Initial commit

---
+++ android/app/src/main/kotlin/com/example/word/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.word
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/word.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/material.dart';
+
+class Word extends StatelessWidget {
+  const Word({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+
+    );
+  }
+}
