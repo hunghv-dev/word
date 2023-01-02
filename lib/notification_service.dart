22fb8c02becca2a1aac12cfa105f336b5cce3e65
hunghv <hunghv@vpbanks.com.vn>
2023-01-02 18:59:05 +0700

    update

--- lib/notification_service.dart
+++
@@ -1,9 +0,0 @@
-import 'package:flutter_local_notifications/flutter_local_notifications.dart';
-
-abstract class NotificationService {
-  void init(Future<dynamic> Function(int, String?, String?, String?)? onDidReceive);
-  Future selectNotification(String? payload);
-  void cancelAllNotifications();
-  void handleApplicationWasLaunchedFromNotification(String payload);
-  Future<List<PendingNotificationRequest>> getAllScheduledNotifications();
-}
