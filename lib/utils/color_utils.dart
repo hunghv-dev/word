11aa9a6429903aac52c8d1c2cb107d4baa55b31a
hunghv <hunghv@vpbanks.com.vn>
2023-01-06 14:58:45 +0700

    update

--- lib/utils/color_utils.dart
+++ lib/utils/color_utils.dart
@@ -2,11 +2,8 @@ import 'package:flutter/material.dart';
 
 class ColorUtils {
   static const Color background = Color(0xff0F202F);
-  static const Color focusBackground = Color(0xff09161e);
-  static const Color black = Colors.black;
   static const Color red = Colors.red;
   static const Color green = Colors.green;
-  static final Color darkGreen = Colors.green.shade900;
   static const Color blue = Colors.blue;
   static final Color grey = Colors.grey.shade400;
 
