d0c6431a85df8ed725995830cdc4b8477e2eae71
hunghv <hunghv@vpbanks.com.vn>
2024-02-20 09:50:56 +0700

    update

--- lib/widgets/item_stepper.dart
+++ lib/widgets/item_stepper.dart
@@ -10,41 +10,39 @@ class ItemStepper extends StatelessWidget {
   const ItemStepper({super.key, required this.index, required this.title});
 
   @override
-  Widget build(BuildContext context) {
-    return Padding(
-      padding: EdgeInsets.zero.top5.horizontal20,
-      child: Column(
-        crossAxisAlignment: CrossAxisAlignment.start,
-        children: [
-          Container(
-              width: 20,
-              height: 20,
-              alignment: Alignment.center,
-              decoration: const BoxDecoration(
-                  shape: BoxShape.circle, color: Colors.blue),
-              child: Text(index)),
-          Stack(
-            children: [
-              Container(
-                width: double.infinity,
-                height: 40,
-                alignment: Alignment.centerLeft,
-                padding: EdgeInsets.zero.left15,
-                margin: EdgeInsets.zero.left10.top5,
-                child: title,
-              ),
-              Positioned(
-                left: 10,
-                bottom: 0,
-                child: Container(
-                    width: 1,
-                    height: 40,
-                    color: ColorsDefine.black().of(context)),
-              ),
-            ],
-          ),
-        ],
-      ),
-    );
-  }
+  Widget build(BuildContext context) => Padding(
+        padding: EdgeInsets.zero.top5.horizontal20,
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            Container(
+                width: 20,
+                height: 20,
+                alignment: Alignment.center,
+                decoration: const BoxDecoration(
+                    shape: BoxShape.circle, color: Colors.blue),
+                child: Text(index)),
+            Stack(
+              children: [
+                Container(
+                  width: double.infinity,
+                  height: 40,
+                  alignment: Alignment.centerLeft,
+                  padding: EdgeInsets.zero.left15,
+                  margin: EdgeInsets.zero.left10.top5,
+                  child: title,
+                ),
+                Positioned(
+                  left: 10,
+                  bottom: 0,
+                  child: Container(
+                      width: 1,
+                      height: 40,
+                      color: ColorsDefine.black().of(context)),
+                ),
+              ],
+            ),
+          ],
+        ),
+      );
 }
